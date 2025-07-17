e4cb045e98ad3458b5f0436bd64665128c0e6a76
Uli2223 <231210063@student.mercubuana-yogya.ac.id>
2025-07-17 22:44:56 +0700

    Initial praktikum

---
+++ database/migrations/2025_07_17_153703_create_produks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->string('id_produk')->primary();
+            $table->string('nama_produk');
+            $table->string('jenis_produk');
+            $table->decimal('harga', 10, 2);
+            $table->integer('stok');
+            $table->date('tanggal_kadaluwarsa');
+            $table->text('deskripsi_produk')->nullable();
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produks');
+    }
+};
